bcc69cd9fc77ad12afcde3f03c2f80397814887c
Piotr (DaAmazingShit) <da.amazing.shit@interia.pl>
2018-08-01 12:10:13 +0200

    Cleanup and some fixes

--- src/pl/betacraft/hardcore/HardcoreBlock.java
+++ src/pl/betacraft/hardcore/HardcoreBlock.java
@@ -2,7 +2,6 @@ package pl.betacraft.hardcore;
 
 import java.util.Random;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -11,37 +10,45 @@ import org.bukkit.block.Block;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockListener;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.material.Jukebox;
 
 public class HardcoreBlock extends BlockListener {
 
 	public void onBlockPlace(BlockPlaceEvent e) {
-		if (e.getBlock().getType() != Material.SPONGE) {
+		// SpongeBlock
+
+		if (e.getBlock().getType() == Material.SPONGE) {
+			Hdb.addSponge(e.getBlock());
 			return;
 		}
-		Hdb.addSponge(e.getBlock());
-		
-		//Moat Digger
-		
-		if(e.getBlockPlaced().getType() == Material.JUKEBOX) {
-            int x = e.getBlockPlaced().getLocation().getBlockX();
-            int y = e.getBlockPlaced().getLocation().getBlockY();
-            int z = e.getBlockPlaced().getLocation().getBlockZ();
-            World w = Bukkit.getWorld("hardcore");
-            Location loc = new Location(w, x, y, z);
-            loc.getBlock().setType(Material.AIR);
-            
-            for (int y1 = e.getBlockPlaced().getY(); y1 > 0; y1--) {
-                Block b = e.getBlockPlaced().getWorld().getBlockAt(e.getBlockPlaced().getX(), y1, e.getBlockPlaced().getZ());
-                if (b.getType() != Material.BEDROCK) {
-                    b.setType(Material.AIR);
-                }
-            }
- 
+
+		// Moat Digger
+
+		if (e.getBlockPlaced().getType() == Material.JUKEBOX) {
+			int x = e.getBlockPlaced().getX();
+			int y = e.getBlockPlaced().getY();
+			int z = e.getBlockPlaced().getZ();
+			World w = e.getBlockPlaced().getWorld();
+			boolean isHardcore = false;
+			for (String world: Hdb.getHardcoreWorlds()) {
+				if (world.equalsIgnoreCase(w.getName())) {
+					isHardcore = true;
+				}
+			}
+			if (!isHardcore) {
+				return;
+			}
+			Location loc = new Location(w, x, y, z);
+			loc.getBlock().setType(Material.AIR);
+
+			for (int y1 = e.getBlockPlaced().getY(); y1 > 0; y1--) {
+				Block b = e.getBlockPlaced().getWorld().getBlockAt(e.getBlockPlaced().getX(), y1, e.getBlockPlaced().getZ());
+				if (b.getType() != Material.BEDROCK) {
+					b.setType(Material.AIR);
+				}
+			}
+			return;
 		}
-		
 	}
 
 	public void onBlockBreak(BlockBreakEvent e) {
@@ -49,11 +56,11 @@ public class HardcoreBlock extends BlockListener {
 			Hdb.removeSponge(e.getBlock());
 			return;
 		}
-		
-		//CobbleX
-		
+
+		// CobbleX
+
 		if (e.getBlock().getType() == Material.STONE && e.getPlayer().getLocation().getBlockY() <= 20) {
-			
+
 			ItemStack diamond = new ItemStack(Material.DIAMOND);
 			ItemStack gold = new ItemStack(Material.GOLD_ORE);
 			ItemStack iron = new ItemStack(Material.IRON_ORE);
@@ -61,23 +68,23 @@ public class HardcoreBlock extends BlockListener {
 			ItemStack tnt = new ItemStack(Material.TNT);
 			ItemStack redstone = new ItemStack(Material.REDSTONE);
 			ItemStack obsidian = new ItemStack(Material.OBSIDIAN);
-			
+
 			Random rand = new Random();
 			int n = rand.nextInt(10) + 1;
-		
-			if(n == 1) {
+
+			if (n == 1) {
 				e.getPlayer().getInventory().addItem(diamond);
 				e.getPlayer().sendMessage(ChatColor.AQUA + "Dostales diament!");
 			}
-			
-			else if(n == 2) {
+
+			else if (n == 2) {
 				e.getPlayer().getInventory().addItem(gold);
 				e.getPlayer().sendMessage(ChatColor.GOLD + "Dostales zloto!");
 				e.getPlayer().getInventory().addItem(redstone);
 				e.getPlayer().sendMessage(ChatColor.RED + "Dostales redstone!");
 			}
-			
-			else if(n == 3) {
+
+			else if (n == 3) {
 				e.getPlayer().getInventory().addItem(iron);
 				e.getPlayer().sendMessage(ChatColor.GRAY + "Dostales zelazo!");
 				e.getPlayer().getInventory().addItem(tnt);
@@ -86,39 +93,37 @@ public class HardcoreBlock extends BlockListener {
 				e.getPlayer().sendMessage(ChatColor.LIGHT_PURPLE + "Dostales obsidian!");
 				
 			}
-			
-			else if(n == 4) {
+
+			else if (n == 4) {
 				e.getPlayer().getInventory().addItem(coal);
 				e.getPlayer().sendMessage(ChatColor.BLACK + "Dostales wegiel!");
 			}
-			
+
 			else {
 				return;
 			}
-			
-			
 		}
 		
 		if (e.getBlock().getType() == Material.STONE && e.getPlayer().getLocation().getBlockY() >= 20) {
-			
+
 			ItemStack gold = new ItemStack(Material.GOLD_ORE);
 			ItemStack iron = new ItemStack(Material.IRON_ORE);
 			ItemStack coal = new ItemStack(Material.COAL);
 			ItemStack tnt = new ItemStack(Material.TNT);
 			ItemStack redstone = new ItemStack(Material.REDSTONE);
 			ItemStack obsidian = new ItemStack(Material.OBSIDIAN);
-			
+
 			Random rand = new Random();
 			int n = rand.nextInt(10) + 1;
-			
-			if(n == 1) {
+
+			if (n == 1) {
 				e.getPlayer().getInventory().addItem(gold);
 				e.getPlayer().sendMessage(ChatColor.GOLD + "Dostales zloto!");
 				e.getPlayer().getInventory().addItem(redstone);
 				e.getPlayer().sendMessage(ChatColor.RED + "Dostales redstone!");
 			}
-			
-			else if(n == 2) {
+
+			else if (n == 2) {
 				e.getPlayer().getInventory().addItem(iron);
 				e.getPlayer().sendMessage(ChatColor.GRAY + "Dostales zelazo!");
 				e.getPlayer().getInventory().addItem(tnt);
@@ -126,16 +131,15 @@ public class HardcoreBlock extends BlockListener {
 				e.getPlayer().getInventory().addItem(obsidian);
 				e.getPlayer().sendMessage(ChatColor.LIGHT_PURPLE + "Dostales obsidian!");
 			}
-			
-			else if(n == 3) {
+
+			else if (n == 3) {
 				e.getPlayer().getInventory().addItem(coal);
 				e.getPlayer().sendMessage(ChatColor.BLACK + "Dostales wegiel!");
 			}
-			
+
 			else {
 				return;
 			}
 		}
 	}
-	
 }
